62306784c85ddd442421929af6db02ed0e6c85c7
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-18 17:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   const array = [];
   
   for (let i = 0; i < musicians.length; i++) {
-    let string = `${musicians[i]} plays ${instruments[i]}`;
+    const string = `${musicians[i]} plays ${instruments[i]}`;
     array.push(string);
   }
   
